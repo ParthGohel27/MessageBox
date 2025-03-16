690f273ccf2a06362fcc364115dd779f3031b903
Parth Gohel <parthgohel2711@gmail.com>
2025-03-16 21:25:50 +0530

    Updated

--- MessageBox/CustomMessageBox.cs
+++ MessageBox/CustomMessageBox.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,100 +8,97 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MessageBox
+namespace CollageManagement
 {
-    public partial class CustomMessageBox: Form
+    public partial class Message: Form
     {
-        public CustomMessageBox()
+        public Message()
         {
             InitializeComponent();
         }
 
-        static CustomMessageBox M;
+        static Message M;
         static String Button_id;
 
-        public static String View(String Message, String Title, String Btn, String Image)
+        public static String Show(String Message, String Title, String Btn, String Image)
         {
-            //Difine Object of This Form
-            M = new CustomMessageBox();
-            //Set Message Text Pass From Calling Method
-            //LMessage is Name of Label
-            M.LMessage.Text = Message;
-            //Set Title Text Pass From Calling Method
-            //LTitle is Name of Label
-            M.LTitle.Text = Title;
-            //Set Button Text Pass From Calling Method
+
+            M = new Message();
+            M.Text.Text = Message;
+            M.Title.Text = Title;
             if (Btn == "YesNo")
             {
-                //button1,button2,button3 is Name of Button
-                M.button1.Text = "Yes";
-                //Set Button Location As X and Y
-                M.button1.Location = new Point(105, 149);
-                M.button2.Text = "No";
-                M.button2.Location = new Point(224, 149);
-                M.button3.Visible = false;
+                M.Btn1.Text = "Yes";
+                M.Btn1.Location = new Point(65, 141);
+                M.Btn2.Text = "No";
+                M.Btn2.Location = new Point(239, 141);
+                M.Btn3.Visible = false;
 
             }
             else if (Btn == "Ok")
             {
-                M.button1.Text = "Ok";
-                M.button1.Location = new Point(167, 149);
-                M.button2.Visible = false;
-                M.button3.Visible = false;
+                M.Btn1.Text = "Ok";
+                M.Btn1.Location = new Point(160, 141);
+                M.Btn2.Visible = false;
+                M.Btn3.Visible = false;
             }
             else if (Btn == "AbortRetryCancel")
             {
-                M.button1.Text = "Abort";
-                M.button2.Text = "Retry";
-                M.button3.Text = "Cancel";
+                M.Btn1.Text = "Abort";
+                M.Btn1.Location = new Point(29, 141);
+                M.Btn2.Text = "Retry";
+                M.Btn2.Location = new Point(160, 141);
+                M.Btn3.Text = "Cancel";
+                M.Btn3.Location = new Point(291, 141);
             }
             else if (Btn == "RetryCancel")
             {
-                M.button1.Text = "Retry";
-                M.button1.Location = new Point(105, 149);
-                M.button2.Text = "Cancel";
-                M.button1.Location = new Point(224, 149);
-                M.button3.Visible = false;
+                M.Btn1.Text = "Retry";
+                M.Btn1.Location = new Point(65, 141);
+                M.Btn2.Text = "Cancel";
+                M.Btn1.Location = new Point(239, 141);
+                M.Btn3.Visible = false;
             }
             else if (Btn == "YesNoCancel")
             {
-                M.button1.Text = "Yes";
-                M.button2.Text = "No";
-                M.button2.Text = "Cancel";
+                M.Btn1.Text = "Yes";
+                M.Btn1.Location = new Point(29, 141);
+                M.Btn2.Text = "No";
+                M.Btn2.Location = new Point(160, 141);
+                M.Btn3.Text = "Cancel";
+                M.Btn3.Location = new Point(291, 141);
             }
             else if (Btn == "OkCancel")
             {
-                M.button1.Text = "Ok";
-                M.button1.Location = new Point(105, 149);
-                M.button2.Text = "Cancel";
-                M.button1.Location = new Point(224, 149);
-                M.button2.Visible = false;
+                M.Btn1.Text = "Ok";
+                M.Btn1.Location = new Point(88, 152);
+                M.Btn2.Text = "Cancel";
+                M.Btn1.Location = new Point(224, 152);
+                M.Btn3.Visible = false;
             }
-            //Image is PictureBox Name
-            //Set Image Path And Take Image Name as a String For Show Image
-            M.Image.Image = new Bitmap("C:\\Users\\dell\\source\\repos\\MessageBox\\MessageBox\\Resources\\" + Image + ".png");
+            M.Image.Image = new Bitmap("C:\\Users\\dell\\source\\repos\\CollageManagement\\CollageManagement\\Resources\\" + Image + ".png");
             M.ShowDialog();
             return Button_id;
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        
+        private void Btn1_Click(object sender, EventArgs e)
         {
-            if (button1.Text == "Yes")
+            if (Btn1.Text == "Yes")
             {
                 Button_id = "1";
                 M.Dispose();
             }
-            else if (button1.Text == "Ok")
+            else if (Btn1.Text == "Ok")
             {
                 Button_id = "4";
                 M.Dispose();
             }
-            else if (button2.Text == "Abort")
+            else if (Btn2.Text == "Abort")
             {
                 Button_id = "5";
                 M.Dispose();
             }
-            else if (button2.Text == "Retry")
+            else if (Btn2.Text == "Retry")
             {
                 Button_id = "6";
                 M.Dispose();
@@ -113,19 +110,19 @@ namespace MessageBox
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Btn2_Click(object sender, EventArgs e)
         {
-            if (button1.Text == "No")
+            if (Btn1.Text == "No")
             {
                 Button_id = "2";
                 M.Dispose();
             }
-            else if (button1.Text == "Cancel")
+            else if (Btn1.Text == "Cancel")
             {
                 Button_id = "3";
                 M.Dispose();
             }
-            else if (button2.Text == "Retry")
+            else if (Btn2.Text == "Retry")
             {
                 Button_id = "6";
                 M.Dispose();
@@ -137,14 +134,14 @@ namespace MessageBox
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Btn3_Click(object sender, EventArgs e)
         {
-            if (button1.Text == "Cancel")
+            if (Btn1.Text == "Cancel")
             {
                 Button_id = "3";
                 M.Dispose();
             }
-            else if (button2.Text == "Ignore")
+            else if (Btn2.Text == "Ignore")
             {
                 Button_id = "7";
                 M.Dispose();
@@ -154,11 +151,6 @@ namespace MessageBox
                 Button_id = "8";
                 M.Dispose();
             }
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
